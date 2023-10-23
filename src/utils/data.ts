324f58198e4807671c49b9531c38201214459bd7
Mamadou Mb. Dione <47165947+mbayedione10@users.noreply.github.com>
2023-10-23 17:25:30 +0000

    ✨ feat (Technologies): List

---
+++ src/utils/data.ts
@@ -0,0 +1,100 @@
+const technologies = [
+    {
+        name: 'Astro',
+        imgSrc: 'https://bestofjs.org/logos/astro.svg',
+        altSrc: 'Astro Logo',
+        link: 'https://astro.build/',
+        ariaLink: 'Astro Documentation Website',
+    },
+    {
+        name: 'JavaScript',
+        imgSrc:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/480px-JavaScript-logo.png',
+        altSrc: 'JavaScript Logo',
+        link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+        ariaLink: 'JavaScript Documentation Website',
+    },
+    {
+        name: 'Supabase',
+        imgSrc: 'https://www.vectorlogo.zone/logos/supabase/supabase-icon.svg',
+        altSrc: 'Supabase Logo',
+        link: 'https://supabase.com/',
+        ariaLink: 'Supabase Documentation Website',
+    },
+    {
+        name: 'Python',
+        imgSrc:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/768px-Python-logo-notext.svg.png',
+        altSrc: 'Python Logo',
+        link: 'https://www.python.org/',
+        ariaLink: 'Python Documentation Website',
+    },
+    {
+        name: 'React',
+        imgSrc:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png',
+        altSrc: 'React Logo',
+        link: 'https://reactjs.org/',
+        ariaLink: 'React Documentation Website',
+    },
+    {
+        name: 'TypeScript',
+        imgSrc:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png',
+        altSrc: 'TypeScript Logo',
+        link: 'https://www.typescriptlang.org/',
+        ariaLink: 'TypeScript Documentation Website',
+    },
+    {
+        name: 'Tailwind CSS',
+        imgSrc:
+            'https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg',
+        altSrc: 'Tailwind CSS Logo',
+        link: 'https://tailwindcss.com/',
+        ariaLink: 'TailwindCSS Documentation Website',
+    },
+    {
+        name: 'Next.js',
+        imgSrc: 'https://static.netify.ai/logos/v/e/r/irepry/icon.png?v=1',
+        altSrc: 'Next.js Logo',
+        link: 'https://nextjs.org/',
+        ariaLink: 'Next.js Documentation Website',
+    },
+    {
+        name: 'Go',
+        imgSrc:
+            'https://geekole.com/wp-content/uploads/2022/12/golang_logo_gopher_geekole.png',
+        altSrc: 'Golang Logo',
+        link: 'https://go.dev/',
+        ariaLink: 'Golang Documentation Website',
+    },
+]
+
+const socials = [
+    {
+        name: 'Twitter',
+        url: 'https://twitter.com/mbayedione10',
+        imgSrc: 'mdi:twitter',
+        ariaLabel: 'Mamadou Mb. DIONE Twitter Profile',
+    },
+    {
+        name: 'GitHub',
+        url: 'https://github.com/mbayedione10',
+        imgSrc: 'mdi:github',
+        ariaLabel: 'Mamadou Mb. DIONE GitHub Profile',
+    },
+    {
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/mamadou-mbaye-dione/',
+        imgSrc: 'mdi:linkedin',
+        ariaLabel: 'Mamadou Mb. DIONE LinkedIn Profile',
+    },
+    {
+        name: 'Discord',
+        url: 'https://discordapp.com/users/807221365467381760',
+        imgSrc: 'ri:discord-fill',
+        ariaLabel: 'Mamadou Mb. DIONE Discord Profile',
+    },
+]
+
+export { technologies, socials }
